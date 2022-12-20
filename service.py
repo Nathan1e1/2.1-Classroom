447887fa034d14c1552318b21d788624bdc58d52
Nathan1e1 <nathaniel.abbott@gmail.com>
2022-12-20 11:52:51 +0000

    update3

--- service.py
+++ service.py
@@ -2,6 +2,37 @@
 # Typically there will be a function for each process that is required, and these will take in data and return data
 
 # Not a complete function, but a suggestion of what to do
-def read_by_id(id):
-    order_data = db.query(id)
-    order = order(order_data) 
+# def read_by_id(id):
+#     order_data = db.query(id)
+#     order = order(order_data) 
+
+from db import connectDB
+
+class services():
+
+    def __init__(self, cursor):
+        self.cursor = cursor
+    
+    def runQuery(self, query):
+        data = self.cursor.execute(query)
+        return
+
+    def addOrder(self):
+        query = "INSERT INTO orders (customer_name, drink, temperature, size, price)"
+        self.runQuery(query)
+        return True
+
+    def delOrder(self, id):
+        query = f"DELETE FROM orderes WHERE order_id = {id}"
+        self.runQuery(query)
+        return True
+
+    def viewAllOrders(self):
+        query = "SELECT * FROM orders"
+        data = self.runQuery(query)
+        return data.fetchall()
+
+    def viewOrderId(self, id):
+        query = f"SELECT * FROM orders WHERE order id = {id}"
+        self.runQuery(query)
+        return True
